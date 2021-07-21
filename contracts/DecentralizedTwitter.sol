778287423451095df0595df47c17731dc4190319
bryg217 <bryan.guillen217@gmail.com>
2021-07-21 17:55:00 -0400

    initial commit

---
+++ contracts/DecentralizedTwitter.sol
@@ -0,0 +1,41 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+contract DecentralizedTwitter {
+  /******************************
+   * State
+   ******************************/
+
+  string public name = "DecentralizedTwitter";
+  mapping (address => User) users;
+
+  /******************************
+   * Structs
+   ******************************/
+
+  /**
+   * @description struct representing a user;
+   * for sake of simplicity, the address is used
+   * for username
+   */
+  struct User {
+    uint userId;
+    address username;
+    bool exists;
+  }
+
+  /******************************
+   * Methods
+   ******************************/
+
+  function createUser(uint userId) public {
+    address sender = msg.sender;
+    require(users[sender].exists == false);
+    User memory newUser = User(userId, sender, true);
+    users[sender] = newUser;
+  }
+
+  function getUser() public view returns (uint userId, address username, bool exists) {
+    User memory user = users[msg.sender];
+    return (user.userId, user.username, user.exists);
+  }
+}
---
+++ test/decentralized-twitter.spec.js
@@ -0,0 +1,52 @@
+const DecentralizedTwitter = artifacts.require("./DecentralizedTwitter.sol");
+
+contract("DecentralizedTwitter", accounts => {
+  describe("getUser", () => {
+    let decentralizedTwitter;
+    
+    before(async () => {
+      decentralizedTwitter = await DecentralizedTwitter.deployed();
+      await decentralizedTwitter.createUser(0);
+    });
+    
+    it("should get user if user exists", async () => {
+      const user = await decentralizedTwitter.getUser();
+      assert.equal(user.username, accounts[0]);
+      assert.equal(user.userId, 0);
+    });
+    
+    it("should get user if user exists", async () => {
+      const secondAccount = accounts[1];
+      const user = await decentralizedTwitter.getUser({ from: secondAccount });
+      const userFound = accounts.find(account => account === user.username) !== undefined;
+      assert.notOk(userFound);
+    });
+  });
+
+  describe("createUser", () => {
+    let decentralizedTwitter;
+    
+    beforeEach(async () => {
+      decentralizedTwitter = await DecentralizedTwitter.deployed();
+    });
+    
+    it("should add user to users if all id is valid and user has not been added yet", async () => {
+      const secondAccount = accounts[1];
+      const USER_ID = 1;
+      await decentralizedTwitter.createUser(USER_ID, { from: secondAccount });
+      const user = await decentralizedTwitter.getUser({ from: secondAccount });
+      assert.equal(user.username, accounts[1]);
+      assert.equal(user.userId, USER_ID);
+    });
+    
+    it("should not add user to users user is already added", async () => {
+      const secondAccount = accounts[1];
+      const USER_ID = 1;
+      try {
+        await decentralizedTwitter.createUser(USER_ID, { from: secondAccount });
+      } catch (error) {
+        assert.ok(error);
+      }
+    });
+  });
+});
