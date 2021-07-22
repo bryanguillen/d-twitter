ef8ec55d2fab33ae211dfe1fe66fa078c7b993ee
bryg217 <bryan.guillen217@gmail.com>
2021-07-22 18:43:21 -0400

    feat: add support for getting recent posts

--- contracts/DecentralizedTwitter.sol
+++ contracts/DecentralizedTwitter.sol
@@ -54,6 +54,27 @@ contract DecentralizedTwitter {
     users[sender] = newUser;
   }
 
+  function getRecentPosts(int idForLastPostSeen) public view returns (int[] memory postIds, uint[] memory userIds) {
+    uint index = findIndexForPost(idForLastPostSeen);
+    uint numberOfPosts = getNumberOfPosts(index); // used to get the length for the memory arrays =(
+
+    uint counter = 0;
+    postIds = new int[](numberOfPosts);
+    userIds = new uint[](numberOfPosts);
+
+    while (counter < 10) {
+      postIds[counter] = posts[index].postId;
+      userIds[counter] = posts[index].userId;
+      counter = counter + 1;
+      index = index - 1;
+      if (index == 0) {
+        break;
+      }
+    }
+
+    return (postIds, userIds);
+  }
+
   function getPost(int idForPostBeingSearched) public view returns (int postId, uint userId) {
     Post memory post = Post(-1, 0); // default
 
@@ -69,5 +90,35 @@ contract DecentralizedTwitter {
   function getUser() public view returns (uint userId, address username, bool exists) {
     User memory user = users[msg.sender];
     return (user.userId, user.username, user.exists);
+  }
+
+  /******************************
+   * Helpers
+   ******************************/
+
+  function findIndexForPost(int idForLastPostSeen) private view returns (uint index) {
+    if (idForLastPostSeen == -1) {
+      index = posts.length - 1;
+    } else {
+      for (uint i = 0; i < posts.length; i++) {
+        if (idForLastPostSeen == posts[i].postId) {
+          index = i - 1;
+          break;
+        }
+      }
+    }
+  }
+
+  function getNumberOfPosts(uint indexForPost) private pure returns (uint numberOfPosts) {
+    numberOfPosts = 0;
+    uint i = indexForPost;
+
+    while (numberOfPosts < 10) {
+      numberOfPosts = numberOfPosts + 1;
+      i = i - 1;
+      if (i == 0) {
+        break;
+      }
+    }
   }
 }
