91b86aa521ed451a3f61582b5ef4c913ece37336
bryg217 <bryan.guillen217@gmail.com>
2021-07-24 21:26:07 -0400

    feat: add boilerplate code for loading blockchain data, smart contract, and user data from smart contract

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,12 @@ import {
 import Home from './pages/home/Home';
 import Loading from './pages/loading/Loading';
 import getWeb3 from './utils/get-web-3';
+import DecentralizedTwitter from './contracts/DecentralizedTwitter.json';
 import './App.css';
 
 function App() {
   const [accountData, setAccountData] = useState({ loginAttempted: false, address: '' });
+  const [decentralizedTwitter, setDecentralizedTwitter] = useState(null);
   const [web3, setWeb3] = useState(null);
 
   /**
@@ -33,6 +35,40 @@ function App() {
       }
     })();
   }, [web3]);
+
+  /**
+   * @description Used to load blockchain data
+   */
+  useEffect(() => {
+    (async function() {
+      const { address, loginAttempted } = accountData;
+  
+      if (loginAttempted && address) {
+        const networkId = await web3.eth.net.getId();
+        const networkData = DecentralizedTwitter.networks[networkId];
+        if(networkData) {
+          const dTwitter = new web3.eth.Contract(DecentralizedTwitter.abi, networkData.address);
+          setDecentralizedTwitter(() => dTwitter);
+        }
+      }
+    })();
+  }, [accountData]);
+
+  /**
+   * @description Used to now interact with block chain and load application data
+   */
+  useEffect(() => {
+    (async function() {
+      if (decentralizedTwitter) {
+        const userData = await decentralizedTwitter.methods.getUser().call();
+        if (!userData.exists) {
+          // @TODO create user
+        } else {
+          // @TODO set user
+        }
+      }
+    })();
+  }, [decentralizedTwitter]);
 
   return (
     <Router>
