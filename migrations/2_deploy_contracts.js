ae00ec039aa6aea9690b9ba85b73048e662de4fa
bryg217 <bryan.guillen217@gmail.com>
2021-07-25 14:19:55 -0400

    chore: remove deleted smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 var DecentralizedTwitter = artifacts.require("./DecentralizedTwitter.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(DecentralizedTwitter);
 };
