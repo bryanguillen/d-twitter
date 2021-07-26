406f78b0cecd6b76477735b2568660d1fe965790
bryg217 <bryan.guillen217@gmail.com>
2021-07-26 11:01:37 -0400

    feat: add initialization for smart contract

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import DecentralizedTwitter from './contracts/DecentralizedTwitter.json';
 import getWeb3 from './utils/get-web-3';
 import Home from './pages/home/Home';
 import initializeAppStores from './utils/initialize-app-stores';
@@ -11,11 +12,13 @@ import Loading from './pages/loading/Loading';
 import './App.css';
 
 function App() {
+  const [decentralizedTwitterContract, setDecentralizedTwitterContract] = useState(null);
   const [stores, setStores] = useState({ user: null, post: null, initialized: false });
   const [web3, setWeb3] = useState(null);
 
   /**
-   * @description Used to initialize application
+   * @description Used to initialize application; NOTE: THIS ASSUMES USER
+   * HAS PROPER SETUP
    */
   useEffect(() => {
     (async function() {
@@ -23,7 +26,12 @@ function App() {
         const { post, user } = await initializeAppStores('http://localhost:5002');
         setStores({ post, user, initialized: true });
 
-        setWeb3(await getWeb3());
+        const web3 = await getWeb3();
+        setWeb3(web3);
+
+        const networkId = await web3.eth.net.getId();
+        const networkData = DecentralizedTwitter.networks[networkId];
+        setDecentralizedTwitterContract(new web3.eth.Contract(DecentralizedTwitter.abi, networkData.address));
       } catch (error) {
         console.log(error);
       }
