64d5c8656e7e708d4b711492d105e2c0f55ead53
bryg217 <bryan.guillen217@gmail.com>
2021-07-26 11:33:20 -0400

    feat: add support for connect button

--- client/src/components/navigation-bar/NavigationBar.jsx
+++ client/src/components/navigation-bar/NavigationBar.jsx
@@ -4,13 +4,16 @@ import { IoHomeSharp } from 'react-icons/io5';
 import './NavigationBar.css';
 
 export default function NavigationBar({
-  handleClickOnHome
+  handleClickOnConnect,
+  handleClickOnHome,
+  loggedIn
 }) {
   return (
     <div className="navigation-bar">
       <IconContext.Provider value={{ size: '2.5rem' }}>
         <IoHomeSharp  className="navigation-bar-home-button" onClick={handleClickOnHome}/>
       </IconContext.Provider>
+      {!loggedIn ? <button className="navigation-bar-connect-button" onClick={handleClickOnConnect}>Connect</button> : null}
     </div>
   );
 }
