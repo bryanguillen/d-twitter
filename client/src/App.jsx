623004aefc82b7b8a9203bfb1d8b434a6c26a57a
bryg217 <bryan.guillen217@gmail.com>
2021-07-26 10:36:26 -0400

    refactor: cleanup initialization code to consist of getting web3 and user db for now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,77 +4,35 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import { initializeOrbitDb } from './utils/orbit-db-utils';
+import getWeb3 from './utils/get-web-3';
 import Home from './pages/home/Home';
 import Loading from './pages/loading/Loading';
-import getWeb3 from './utils/get-web-3';
-import DecentralizedTwitter from './contracts/DecentralizedTwitter.json';
 import './App.css';
 
 function App() {
-  const [accountData, setAccountData] = useState({ loginAttempted: false, address: '' });
-  const [decentralizedTwitter, setDecentralizedTwitter] = useState(null);
+  const [userDb, setUserDb] = useState(null);
   const [web3, setWeb3] = useState(null);
 
   /**
-   * @description Used to load web3 and account data from web3
+   * @description Used to initialize applciation
    */
   useEffect(() => {
     (async function() {
-      setWeb3(await getWeb3());
+      try {
+        setUserDb(await initializeOrbitDb());
+        setWeb3(await getWeb3());
+      } catch (error) {
+        console.log(error);
+      }
     })();
   }, []);
-
-  /**
-   * @description Used to set account data
-   */
-  useEffect(() => {
-    (async function() {
-      if (web3) {
-        const [address] = await web3.eth.getAccounts();
-        setAccountData({ loginAttempted: true, address }); // perhaps there is an easier way of identifying account or not
-      }
-    })();
-  }, [web3]);
-
-  /**
-   * @description Used to load blockchain data
-   */
-  useEffect(() => {
-    (async function() {
-      const { address, loginAttempted } = accountData;
-  
-      if (loginAttempted && address) {
-        const networkId = await web3.eth.net.getId();
-        const networkData = DecentralizedTwitter.networks[networkId];
-        if(networkData) {
-          const dTwitter = new web3.eth.Contract(DecentralizedTwitter.abi, networkData.address);
-          setDecentralizedTwitter(() => dTwitter);
-        }
-      }
-    })();
-  }, [accountData]);
-
-  /**
-   * @description Used to now interact with block chain and load application data
-   */
-  useEffect(() => {
-    (async function() {
-      if (decentralizedTwitter) {
-        const userData = await decentralizedTwitter.methods.getUser().call();
-        if (!userData.exists) {
-          // @TODO create user
-        } else {
-          // @TODO set user
-        }
-      }
-    })();
-  }, [decentralizedTwitter]);
 
   return (
     <Router>
       <div className="app">
         {
-          accountData.loginAttempted ?
+          userDb && web3 ?
             <Switch>
               <Route path="/">
                 <Home/>
